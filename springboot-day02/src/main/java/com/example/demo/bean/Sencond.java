6bf75e295f517808b4cec9b1e9cf2bfe7812196f
dashuaibi <dashuaibi@163.com>
2021-11-14 23:01:58 +0800

    first commit

---
+++ springboot-day02/src/main/java/com/example/demo/bean/Sencond.java
@@ -0,0 +1,7 @@
+package com.example.demo.bean;
+
+public class Sencond {
+    public static void main(String[] args) {
+        System.out.println("hello Sencond");
+    }
+}
---
+++ springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
@@ -0,0 +1,7 @@
+package com.example.demo.bean;
+
+public class ThirdTy {
+    public static void main(String[] args) {
+        System.out.println("hello ThirdTy");
+    }
+}
