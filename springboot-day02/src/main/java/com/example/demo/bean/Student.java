f745403aa609abe23b33a6c726bd3ba2d445e7f7
dashuaibi <dashuaibi@163.com>
2021-11-14 23:07:46 +0800

    test back

--- springboot-day02/src/main/java/com/example/demo/bean/Student.java
+++ springboot-day02/src/main/java/com/example/demo/bean/Student.java
@@ -2,6 +2,6 @@ package com.example.demo.bean;
 
 public class Student {
     public static void main(String[] args) {
-        System.out.println("hello world first");
-    }
+        System.out.println("hello world first testback");
+        }
 }
