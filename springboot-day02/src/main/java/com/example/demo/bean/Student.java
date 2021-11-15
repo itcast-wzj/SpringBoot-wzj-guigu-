c678309edc7bc3cfa766e70e5ca82069fe60fdd3
dashuaibi <dashuaibi@163.com>
2021-11-15 22:10:21 +0800

    thirdly update master branch

--- springboot-day02/src/main/java/com/example/demo/bean/Student.java
+++ springboot-day02/src/main/java/com/example/demo/bean/Student.java
@@ -5,5 +5,6 @@ public class Student {
         System.out.println("hello world first testback");
         System.out.println("我是master分支");
         System.out.println("我是master分支2");
+        System.out.println("我是master分支3");
         }
 }
