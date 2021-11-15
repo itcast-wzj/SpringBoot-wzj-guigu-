4f0104c7c30203caabbc0d007d8fff8db31c21bc
dashuaibi <dashuaibi@163.com>
2021-11-15 22:05:54 +0800

    first update master branch

--- springboot-day02/src/main/java/com/example/demo/bean/Student.java
+++ springboot-day02/src/main/java/com/example/demo/bean/Student.java
@@ -3,5 +3,6 @@ package com.example.demo.bean;
 public class Student {
     public static void main(String[] args) {
         System.out.println("hello world first testback");
+        System.out.println("我是master分支");
         }
 }
