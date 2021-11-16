23d060e8c076c11f60d45ed1be83a8d04a7415e3
dashuaibi <dashuaibi@163.com>
2021-11-16 11:58:57 +0800

    test stash and stash pop

--- springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
+++ springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
@@ -5,6 +5,6 @@ public class ThirdTy {
         System.out.println("hello ThirdTy");
         System.out.println("准备提交到 本地仓库");
         System.out.println("test git stash");
-        System.out.println("我是住在本地仓库2");
+        System.out.println("我是住在本地仓库2123");
     }
 }
