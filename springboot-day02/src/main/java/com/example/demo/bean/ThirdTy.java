c9ac878ce8088345c5de8455186473738696e9c1
dashuaibi <dashuaibi@163.com>
2021-11-16 11:10:35 +0800

    first test local commit

--- springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
+++ springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
@@ -3,5 +3,7 @@ package com.example.demo.bean;
 public class ThirdTy {
     public static void main(String[] args) {
         System.out.println("hello ThirdTy");
+        System.out.println("准备提交到 本地仓库");
+        System.out.println("我是住在本地仓库");
     }
 }
