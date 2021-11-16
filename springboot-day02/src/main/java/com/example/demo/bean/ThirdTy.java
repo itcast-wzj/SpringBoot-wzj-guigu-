e65c9e5ca064e1d7c88e71a378450789b6323096
itcast_wzj <46273670+itcast-wzj@users.noreply.github.com>
2021-11-16 11:54:18 +0800

    Update ThirdTy.java

--- springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
+++ springboot-day02/src/main/java/com/example/demo/bean/ThirdTy.java
@@ -4,6 +4,7 @@ public class ThirdTy {
     public static void main(String[] args) {
         System.out.println("hello ThirdTy");
         System.out.println("准备提交到 本地仓库");
+        System.out.println("test git stash");
         System.out.println("我是住在本地仓库2");
     }
 }
