beacb7b8d4b82fc2e38024409fd1094532463f0d
dashuaibi <dashuaibi@163.com>
2021-11-17 19:40:27 +0800

    del other file

--- springboot-day02/src/main/java/com/example/demo/bean/Sencond.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.bean;
-
-public class Sencond {
-    public static void main(String[] args) {
-        System.out.println("hello Sencond");
-    }
-}
--- springboot-day02/src/main/java/com/example/demo/bean/Specimal.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.bean;
-
-public class Specimal {
-    public static void main(String[] args) {
-        System.out.println("hello specimal commit branch");
-    }
-}
